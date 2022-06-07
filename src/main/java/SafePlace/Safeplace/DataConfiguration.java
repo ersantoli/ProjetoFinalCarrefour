79c7e6c3ed35317bc9ccdfb967b8d2686e8f0003
ersantoli <74686375+ersantoli@users.noreply.github.com>
2022-06-07 20:28:26 -0300

    Update DataConfiguration.java

--- src/main/java/SafePlace/Safeplace/DataConfiguration.java
+++ src/main/java/SafePlace/Safeplace/DataConfiguration.java
@@ -19,10 +19,10 @@ public class DataConfiguration {
 		
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		
-		dataSource.setUrl("jdbc:postgresql://ec2-3-216-221-31.compute-1.amazonaws.com/dp5ju1u9jcl1r"
+		dataSource.setUrl("jdbc:postgresql://localhost5432/academia"
 				+ "");
-		dataSource.setUsername("fjhcgulfigzokx");
-		dataSource.setPassword("c19302fc85ef058c9c911d036c4ef4571ddd7673d547122aead57530c3cd7d55");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
 		return  dataSource;
 		
 		
